e88c19b19a06316e449b84dd66682dd1b9c60004
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-10-24 11:00:05 -0300

    Dia 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import {Container} from './App.style';
+import {useState} from 'react';
+import {Area, Container, Header} from './App.style';
 
 
 const App = () =>{
+  const [list, setList] = useState();
 return(
   <Container>
-   
+   <Area>
+   <Header>
+     Lista de Tarefas
+   </Header>
+   </Area>
   </Container>
 );
 }
